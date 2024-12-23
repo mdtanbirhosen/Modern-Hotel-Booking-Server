60262905ccf9b7c0feafb509f54810f3a654a592
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2024-12-23 23:59:11 +0600

    vercel deploy done with getting all rooms data

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
@@ -11,6 +10,9 @@ app.use(express.json())
 
 
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q5jln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q5jln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,16 +27,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const database = client.db('HotelDB');
+    const roomsCollection = database.collection('rooms')
+
+    app.get('/rooms',async(req,res)=>{
+      const result = await roomsCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
 
 
 
